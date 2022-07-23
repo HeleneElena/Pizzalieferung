b666cec641f4f993a78e44676f9a8ec4a4a56538
HeleneElena <heleneebert1@gmail.com>
2022-07-23 15:42:43 +0200

    commit2

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const buttonAuth = document.querySelector('.button-auth'),
+      modalAuth = document.querySelector('.modal-auth'),
+      buttonOut = document.querySelector('.button-out'),
+      userName = document.querySelector('.user-name'),
+      closeAuth = document.querySelector('.close-auth'),
+      logInForm = document.getElementById('logInForm'),
+      buttonLogin = document.querySelector('.button-login'),
+      inputLogin = document.getElementById('login'),
+      inputPassword = document.getElementById('password');
+
+
+const login = (user) => {
+    buttonAuth.style.display = 'none';
+    buttonOut.style.display = 'flex';
+    
+    userName.style.display = 'flex';
+    userName.textContent = user.login;
+    modalAuth.style.display = 'none';
+};
+
+const logout = () => {
+    buttonAuth.style.display = 'flex';
+    buttonOut.style.display = 'none';
+    userName.style.display = 'none';
+    userName.textContent = '';
+    localStorage.removeItem('user');
+};
+
+buttonAuth.addEventListener('click', () => {
+    modalAuth.style.display = 'flex';
+});
+
+closeAuth.addEventListener('click', () => {
+    modalAuth.style.display = 'none';
+});
+
+logInForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const user = {
+      login: inputLogin.value,
+      password: inputPassword.value,
+    };
+    localStorage.setItem('user', JSON.stringify(user));
+    login(user);
+});
+
+if(localStorage.getItem('user')) {
+    login(JSON.parse(localStorage.getItem('user')));
+}
+
+buttonOut.addEventListener('click', logout);
